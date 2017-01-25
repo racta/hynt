02e870ed4c93b0f8359788ef640c69caeb01d8b5
racta <pivavoh@nutpa.net>
2017-01-25 21:05:23 +0530

    Update main.go

--- main.go
+++ main.go
@@ -3,7 +3,8 @@ package main
 import (
 	"log"
 
-	"github.com/jpillora/cloud-torrent/server"
+	"github.com/racta/cloud-torrent/server"
+	"github.com/racta/opts"
 )
 
 var VERSION = "0.0.0-src" //set with ldflags
@@ -15,6 +16,7 @@ func main() {
 		ConfigPath: "cloud-torrent.json",
 	}
 
+	o := opts.New(&s)
 	o.Version(VERSION)
 	o.PkgRepo()
 	o.LineWidth = 96
